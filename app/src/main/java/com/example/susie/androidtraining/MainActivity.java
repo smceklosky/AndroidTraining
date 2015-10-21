40205a8247ecad1682ecbccaac2cfeceea39fbb4
Sue Ceklosky <smceklosky@gmail.com>
2015-10-20 21:23:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/susie/androidtraining/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.susie.androidtraining;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameEditText;
+    EditText emailEditText;
+    EditText addressEditText;
+    EditText cityEditText;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        cityEditText = (EditText) findViewById(R.id.cityEditText);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void goToWelcome (View view){
+
+        String name = nameEditText.getText().toString();
+
+        if (name.trim().equals(""))
+            Toast.makeText(this, "Please enter a name!", Toast.LENGTH_LONG).show();
+        else {
+            intent = new Intent(this, WelcomeActivity.class);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menu_home :
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            case R.id.menu_welcome :
+
+                intent = new Intent(this, WelcomeActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
